d3aaeed1eae64c11b780eb26998c32a967b213f2
Veit Progl <mail@veit.dev>
2020-01-23 11:16:48 +0100

    tested navigation

---
+++ Makerlog/View/NavigationbarView.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationbarView.swift
+//  Makerlog
+//
+//  Created by Veit Progl on 22.01.20.
+//  Copyright © 2020 Veit Progl. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationbarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationbarView()
+    }
+}
