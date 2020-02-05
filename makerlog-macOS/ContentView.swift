36aef9742624383bb5668277d03c88e20f7ebe63
Veit Progl <mail@veit.dev>
2020-02-05 09:42:23 +0100

    macOS

---
+++ makerlog-macOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  makerlog-macOS
+//
+//  Created by Veit Progl on 05.02.20.
+//  Copyright © 2020 Veit Progl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
