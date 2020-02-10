bd0e980fa6ce94493ade1d12b793a9b8103a8ab5
Veit Progl <mail@veit.dev>
2020-02-10 11:50:55 +0100

    removed test view

--- Makerlog-iOS/View/tabView.swift
+++ Makerlog-iOS/View/tabView.swift
@@ -36,11 +36,6 @@ struct TabScreen: View {
 			}
             .tabItem({ TabLabel(imageName: "bell.fill", label: "Notification") })
 
-			VStack {
-				ExampleView()
-			}
-            .tabItem({ TabLabel(imageName: "bell.fill", label: "Notification") })
-
         }
 		.sheet(isPresented: self.$data.userSheet, content: {UserView(login: self.login)})
     }
@@ -62,35 +57,4 @@ struct TabScreen_Previews: PreviewProvider {
     static var previews: some View {
         TabScreen()
     }
-}
-
-
-struct ExampleView: View {
-    @State private var show: Bool = false
-    
-    var body: some View {
-        
-        Button("Open Sheet") {
-            self.show = true
-        }
-        .padding(10).border(show ? Color.red : Color.clear)
-        .actionSheet(isPresented: $show, content: aSheet)
-        
-    }
-    
-    func aSheet() -> ActionSheet {
-
-        let send = ActionSheet.Button.default(Text("Send")) { print("hit send") }
-        let draft = ActionSheet.Button.default(Text("Draft")) { print("hit draft") }
-
-        // If the cancel label is omitted, the default "Cancel" text will be shown
-        let cancel = ActionSheet.Button.cancel(Text("Abort")) { print("hit abort") }
-
-        let buttons: [ActionSheet.Button] = [send, draft, cancel]
-        
-        return ActionSheet(title: Text("Title"),
-                           message: Text("Message goes here"),
-                           buttons: buttons)
-    }
-
 }
